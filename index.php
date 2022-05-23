7b8be87ca48663ccaa2e3838c297b9ece0d99b3f
Mateusz Miotk <mmiotk@inf.ug.edu.pl>
2022-05-23 16:24:19 +0200

    Add index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Implementacja sesji</title>
+</head>
+<body>
+<div>
+    Witamy na drugiej stronie sesji.<br/>
+    Identyfikatorem sesji jest:<br/>
+    Została ustawiona zmienna o nazwie: zmienna_sesji<br/>
+    Wartością zmiennej zmienna_sesji jest:<br/>
+</div>
+</body>
+</html>
